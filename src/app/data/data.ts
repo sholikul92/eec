45a28d1a302afc1707493584e353934fad22d9e3
Happy City <happycity805@gmail.com>
2025-07-04 17:14:30 +0700

    update image

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -79,51 +79,111 @@ export const products = [
   {
     id: 1,
     title: 'Meja Kursi MK-01',
-    imageUrl: '/product/mk-01.jpg',
+    imageUrl: '/product/kursi-sekolah/mk-01.jpg',
   },
   {
     id: 2,
     title: 'Meja Kursi MK-02',
-    imageUrl: '/product/mk-02.jpg',
+    imageUrl: '/product/kursi-sekolah/mk-02.jpg',
   },
   {
     id: 3,
     title: 'Meja Kursi MK-03',
-    imageUrl: '/product/mk-03.jpg',
+    imageUrl: '/product/kursi-sekolah/mk-03.jpg',
   },
   {
     id: 4,
     title: 'Meja Kursi MK-04',
-    imageUrl: '/product/mk-04.jpg',
+    imageUrl: '/product/kursi-sekolah/mk-04.jpg',
   },
   {
     id: 5,
     title: 'Meja Kursi MK-05',
-    imageUrl: '/product/mk-05.jpg',
+    imageUrl: '/product/kursi-sekolah/mk-05.jpg',
   },
   {
     id: 6,
     title: 'Meja Kursi MK-06',
-    imageUrl: '/product/mk-06.jpg',
+    imageUrl: '/product/kursi-sekolah/mk-06.jpg',
   },
   {
     id: 7,
     title: 'Meja Kursi MK-07',
-    imageUrl: '/product/mk-07.jpg',
+    imageUrl: '/product/kursi-sekolah/mk-07.jpg',
   },
   {
     id: 8,
     title: 'Meja Kursi MK-08',
-    imageUrl: '/product/mk-08.jpg',
+    imageUrl: '/product/kursi-sekolah/mk-08.jpg',
   },
   {
     id: 9,
     title: 'Meja Kursi MK-09',
-    imageUrl: '/product/mk-09.jpg',
+    imageUrl: '/product/kursi-sekolah/mk-09.jpg',
   },
   {
     id: 10,
     title: 'Meja Kursi MK-10 Biru',
-    imageUrl: '/product/mk-110-biru.jpg',
+    imageUrl: '/product/kursi-sekolah/mk-110-biru.jpg',
+  },
+  {
+    id: 11,
+    title: 'Meja Kursi TK Bulan Biru',
+    imageUrl: '/product/kursi-tk/mktk-bulan-biru.jpg',
+  },
+  {
+    id: 12,
+    title: 'Meja Kursi TK Bulan Kuning',
+    imageUrl: '/product/kursi-tk/mktk-bulan-kuning.jpg',
+  },
+  {
+    id: 13,
+    title: 'Meja Kursi TK Kotak Hijau',
+    imageUrl: '/product/kursi-tk/mktk-kotak-hijau.jpg',
+  },
+  {
+    id: 14,
+    title: 'Meja Kursi TK Kotak Merah',
+    imageUrl: '/product/kursi-tk/mktk-kotak-merah.jpg',
+  },
+  {
+    id: 15,
+    title: 'Meja Kursi TK Kotak Kuning',
+    imageUrl: '/product/kursi-tk/mktk-kotak-kuning.jpg',
+  },
+  {
+    id: 16,
+    title: 'Meja Kursi TK Segi Lima Hijau',
+    imageUrl: '/product/kursi-tk/mktk-segilima-hijau.jpg',
+  },
+  {
+    id: 17,
+    title: 'Meja Kursi TK Segi Lima Kuning',
+    imageUrl: '/product/kursi-tk/mktk-segilima-kuning.jpg',
+  },
+  {
+    id: 18,
+    title: 'Meja Kursi TK Segi Lima Biru',
+    imageUrl: '/product/kursi-tk/mktk-segilima-biru.jpg',
+  },
+  {
+    id: 19,
+    title: 'Meja Kursi TK Bulat Merah',
+    imageUrl: '/product/kursi-tk/mktk-bulat-merah.jpg',
+  },
+  {
+    id: 20,
+    title: 'Meja Kursi TK Bulat Hijau',
+    imageUrl: '/product/kursi-tk/mktk-bulat-hijau.jpg',
+  },
+  {
+    id: 21,
+    title: 'Meja Kursi TK Bulat Kuning',
+    imageUrl: '/product/kursi-tk/mktk-bulat-kuning.jpg',
+  },
+  {
+    id: 22,
+    title: 'Meja Kursi TK Bulat Biru',
+    imageUrl: '/product/kursi-tk/mktk-bulat-biru.jpg',
   },
 ];
